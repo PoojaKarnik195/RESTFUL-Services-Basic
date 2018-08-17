9d3b45192d71c32c9491f7ca2e6da31a097b8cf3
PoojaKarnik195 <41577579+PoojaKarnik195@users.noreply.github.com>
2018-08-17 17:57:01 +0530

    Add files via upload

---
+++ rest2/src/main/java/com/demo2/rest2/service/Greeting.java
@@ -0,0 +1,13 @@
+package com.demo2.rest2.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greeting {
+
+	@RequestMapping("/hello")
+	public String sayHello(String name) {
+		return "Hello "+name;
+	}
+}
